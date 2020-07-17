3dd039bf79b01eebdf1dc239094b280bc3308202
WANG FENGYUE <xa5es@hotmail.com>
2020-07-17 14:26:19 +0800

    Update wireguard_install_ubuntu_gcp.sh

--- wireguard_install_ubuntu_gcp.sh
+++ wireguard_install_ubuntu_gcp.sh
@@ -44,8 +44,7 @@ Address = 192.168.0.1/24
 PostUp   = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -A FORWARD -o wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o $eth -j MASQUERADE
 PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -D FORWARD -o wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o $eth -j MASQUERADE
 ListenPort = $port
-DNS = 8.8.8.8
-MTU = 1420
+DNS = 8.8.8.8, 8.8.4.4
 
 [Peer]
 PublicKey = $c2
